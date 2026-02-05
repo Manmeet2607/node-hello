fa0d5f041b9cdf8461e2eabf00270a5677d3d37d
Manmeet Kaur <70068282+Manmeet2607@users.noreply.github.com>
2026-02-05 20:19:03 +0530

    Update5

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello Nodes52!\n'
+  const msg = 'Hello Nodes525!\n'
   res.end(msg);
 });
 
