9c879996d269b4c07730e4456a240214d026d0ba
Manmeet Kaur <70068282+Manmeet2607@users.noreply.github.com>
2026-02-07 12:49:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,4 @@
-const http = require('http');
-const port = process.env.PORT || 3000;
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  const msg = 'Hello Nodes525!\n'
-  res.end(msg);
-});
-
-server.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}/`);
-});
+function greet(name) {
+  return `Hello, ${name}!`;
+}
+module.exports = { greet };
